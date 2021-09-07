809e6d30bb25561499eb5d4ad85aee819b77146d
Ridwan Suhel <65410320+Ridwan-Suhel@users.noreply.github.com>
2021-09-07 19:36:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let data = [
+    {
+        name: "Ridwan",
+        age: 23
+    },
+    {
+        name: "Kaka",
+        age: 40
+    },
+    {
+        name: "Alex",
+        age: 26
+    },
+    {
+        name: "Romero",
+        age: 19
+    },
+    {
+        name: "John",
+        age: 23
+    },
+    {
+        name: "Nessam",
+        age: 27
+    },
+    {
+        name: "Katy",
+        age: 24
+    },
+]
+
+let info = document.querySelector(".sq-info");
+
+
+let details = data.map(function(item){
+    return "<div>"+ item.name +" is "+ item.age+ " Years old"+"</div>";
+});
+
+info.innerHTML = details.join("\n");
+
+
+
+
+
